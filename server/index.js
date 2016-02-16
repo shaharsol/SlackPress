54a68027f41e5bb65274e4bd8499dc08c187d0a9
Gabriel Axel <guznik@gmail.com>
2016-02-16 11:11:34 +0200

    Added initial “/blogit” endpoint

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -13,8 +15,10 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.post('/blogit', function(request, response) {
+  response.send(request.body);
+})
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
