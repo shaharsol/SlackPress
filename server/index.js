80829b846345db6bd8817cd13570d6c7a348bca2
Shahar Solomianik <shahar@gnift.com>
2016-02-16 11:16:30 +0200

    basic github connection started

--- server/index.js
+++ server/index.js
@@ -1,3 +1,6 @@
+var url = require('url');
+var config = require('config');
+
 var express = require('express');
 var app = express();
 
@@ -8,6 +11,20 @@ app.use(express.static(__dirname + '/public'));
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+
+app.get('/connect-github', function(req, res, next) {
+	var redirect = {
+		protocol: 'https',
+		host: 'github.com',
+		pathname: '/login/oauth/authorize',
+		query: {
+			client_id: config.get('github.client_id'),
+			redirect_uri: 'http://' + config.get('github.redirect_domain') + '/connected-github',
+		}
+	}
+	res.redirect(url.format(redirect));
+});
+
 
 app.get('/', function(request, response) {
   response.render('pages/index');
